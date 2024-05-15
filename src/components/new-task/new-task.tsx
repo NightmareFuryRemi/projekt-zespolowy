eda164a4214b3098e235334461debef8ca87813a
NightmareFuryRemi <remiassek@gmail.com>
2024-05-15 20:44:11 +0200

    new tables task

--- src/components/new-task/new-task.tsx
+++ src/components/new-task/new-task.tsx
@@ -1,18 +1,21 @@
 import React from "react";
 
 const NewTask: React.FC = () => {
+  return (
+    <div className="new-task">
+      <input
+        type="text"
+        className="new-task__title"
+        placeholder="Name a task"
+      ></input>
 
-    return(
-        <div className="new-task">
-            <input
-            type="text"
-            className="new-task-title"
-            placeholder="Name a task">
-            </input>
-        </div>
-    );
-
-
+      <input
+        type="text"
+        className="new-task__desc"
+        placeholder="Describe a task"
+      />
+    </div>
+  );
 };
 
 export default NewTask;
