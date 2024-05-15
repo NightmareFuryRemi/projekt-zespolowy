1f546123521733918bcda1500fbe7d61ddf3fae3
NightmareFuryRemi <remiassek@gmail.com>
2024-05-15 19:49:18 +0200

    base

---
+++ src/components/main-page/main-page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage: React.FC<{}> = () => {
+  return (
+    <div className="main-page">
+      <h1>Witaj na stronie głównej!</h1>
+    </div>
+  );
+};
+
+export default MainPage;
