9ba224a41c650f9d6d9fcd415c73e5c563bb77b3
lwojcik00 <132928265+lwojcik00@users.noreply.github.com>
2024-05-15 20:21:29 +0200

    1st commit

--- src/components/main-page/main-page.tsx
+++ src/components/main-page/main-page.tsx
@@ -6,6 +6,9 @@ const MainPage: React.FC<{}> = () => {
   return (
     <div className="main-page">
       <h1>Witaj na stronie głównej!</h1>
+      <div className="bottom-Page">
+        <button>Button_1</button>
+      </div>
     </div>
   );
 };
